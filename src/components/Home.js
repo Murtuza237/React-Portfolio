1280f2ee15a0f34f4b45cca575c89506a7964a72
Murtuza Ali <murtuzaali@Murtuzas-MacBook-Air.local>
2025-03-25 11:58:09 +0530

    Fixed footer positioning and routing issues for GitHub Pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import "../App.css";
-import prof from "../Images/profile.tiff"
-
+import prof from "../Images/profile.tiff";
 
 const Home = () => {
   return (
     <section id="home">
       <div className='wrapper'>
-      <h1>Welcome to My Portfolio</h1>
-        <img src={prof} className="home-image" alt=""/>
+        <h1>Welcome to My Portfolio</h1>
+        <img src={prof} className="home-image" alt="Murtuza Ali profile" onError={(e) => {
+          e.target.onerror = null;
+          e.target.style.display = 'none';
+        }}/>
         <h2>Hi, I'm Murtuza Ali</h2>
-        <h3>Python Programmer | Web Deveplor | C/C++ | MySql </h3>
+        <h3>Python Programmer | Web Developer | C/C++ | MySQL</h3>
       </div>
     </section>
   );
